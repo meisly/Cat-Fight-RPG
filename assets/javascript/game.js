2a76d41d0d9f668d5eedcf1a720c50ca82cafadf
Tierra <sr.undershaft@gmail.com>
2019-07-08 22:07:33 -0500

    added javascript for win and lose behavior

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,44 +27,42 @@ $(".other-btn").click(function () {
 
 
 class Character {
-    constructor (name, health, baseAttack, counter) {
+    constructor (name, health, baseAttack, counter, id) {
         this.name = name;
         this.health = health;
         this.baseAttack = baseAttack;
         this.counter = counter;
         this.attackMultiplier = 1;
+        this.id = id;
     }
     attack (enemy) {
         const enemyDamage = this.baseAttack * this.attackMultiplier;
         enemy.health -= enemyDamage;
         this.attackMultiplier++;
         this.health -= enemy.counter;
-        $("game-info").html(`
+        $("#game-update").html(`
             <p>You attacked ${enemy.name} for ${enemyDamage} hp. </p>
             <p>${enemy.name} fought back and delivered ${enemy.counter} damage </p>
         `);
-        $("player1-health").html(`You have ${this.health} hp`);
-        $("npc-health").html(`${enemy.name} has ${enemy.health} hp`)
+        $("#game-info").addClass("hidden");
     }
-
+isDed () {
+    if (this.health <=0) {
+        return true;
+    }
+}
     static createCharacter(charVar) {
-
         switch (charVar) {
             case "catTruck": 
-                return new Character ("Cat Truck", 150, 5, 20);
-                
+                return new Character ("Cat Truck", 150, 5, 20, "catTruck");    
             case "catStevens":
-                return new Character ("Cat Stevens", 120, 2, 50);
-                
+                return new Character ("Cat Stevens", 120, 2, 50, "catStevens");
             case "grumpyCat":
-                return new Character ("Grumpy Cat", 70, 10, 15);
-                
+                return new Character ("Grumpy Cat", 70, 10, 15, "grumpyCat");
             case "katO":
-                return new Character ("Katherine", 300, 2, 5);
-                
+                return new Character ("Katherine", 300, 2, 5, "katO");
             case "frenchie":
-                return new Character ("Frenchie", 100, 7, 10);
-                
+                return new Character ("Frenchie", 100, 7, 10, "frenchie");
         }
     }
 }
@@ -73,7 +71,7 @@ function pickChar (charVar) {
     $("#character-bank").toggle();
     $("#character-bank-small").toggle();
     player = Character.createCharacter(charVar);
-    $("#game-info").text("Choose your first oponent!");
+    $("#game-info").text("Choose your first opponent!");
     
 }
 
@@ -105,10 +103,35 @@ $(".char-pic-con-small").click(function () {
     $(charImg).removeClass();
     $(charImg).addClass("fighter-pic");
     $("#cbs-"+char).addClass("active-fighter");
+    $("#attack-btn").toggle();
     pickEnemy(char);
     updateHP()
 });
 
+$("#attack-btn").click(function () {
+    player.attack(enemy);
+    if (player.isDed()) {
+        $("#game-info").removeClass("hidden");
+        $("#attack-btn").toggle();
+        $("#game-info").text("You lost.  You're a terrible cat fighter");
+        var startOver = $("<button>");
+        $("#game-info").append(startOver);
+        startOver.html("Try Again?");
+        startOver.click( function () {
+            location.reload();
+        })
+    }
+    if (enemy.isDed()) {
+        $("#game-info").text(`Congratulations, you have defeated ${enemy.name}! Choose your next oponent.`)
+        $("#game-info").removeClass("hidden");
+        $("#game-update").addClass("hidden");
+        $("#npc").html("");
+        $("#attack-btn").toggle();
+        $("#cbs-"+ enemy.id).addClass("dead-fighter");
+        $(".char-pic-con-small").css("pointer-events", "auto");
+    }
+    updateHP();
 
+});
 
 
